20c1cee31c1e7e476c710ae6d9f4efd6af9104f0
Ralfarios <muamaralfarabi@gmail.com>
2024-10-05 13:18:02 +0700

    fix: access denied for writing file

--- internals/repositories/grocery_repository.go
+++ internals/repositories/grocery_repository.go
@@ -67,7 +67,7 @@ func (repository *GroceryRepository) loadDb() ([]domain.Grocery, error) {
 }
 
 func (repository *GroceryRepository) saveDb(groceries []domain.Grocery) error {
-	file, err := os.Open(repository.filepath)
+	file, err := os.OpenFile(repository.filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return nil
 	}
